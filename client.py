9fb4c452f2b3521330398236c571693869e8c695
KevinGines1 <kagines1@up.edu.ph>
2021-05-19 11:00:25 +0800

    clean up code and add documentations

--- client.py
+++ client.py
@@ -7,17 +7,17 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = input("Please enter the IP address of server: ")
 
 s.connect((host, 5000)) # connect to the specified port (should be the port of the server)
-print(s.recv(1024).decode())
+print(s.recv(1024).decode()) # receive the acknowledgement from the server
 
 while True: 
-    print(s.recv(1024).decode(), end="") # prompt for input
+    print(s.recv(1024).decode(), end="") # server prompts for input
     input_text = input()
     s.send(input_text.encode()) # send input to server
-    print(input_text, " in Pig Latin is: ", s.recv(1024).decode()) # translated 
-    print(s.recv(1024).decode(), end="") # continue? prompt
+    print(input_text, " in Pig Latin is: ", s.recv(1024).decode()) # receive translated word/s
+    print(s.recv(1024).decode(), end="") # server asks to continue? prompt
     proceed = input() # answer to continue? prompt
     s.send(proceed.encode()) # send response to server
 
     if proceed == "n":
-        s.close() # if response is n, we terminate the connection
+        s.close() # if response is n, we terminate the connection to the server
         break # end the loop too
--- server.py
+++ server.py
@@ -3,16 +3,20 @@ import socket
 
 # function that sets up the server
 def setupServer():
-    # create a socket object with TCP
+
+    # create a socket object with IPv6 & TCP
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try: 
         #use port 5000
-        host = ''
+        host = '' # this is blank so we can accept connections from other computers
         port = 5000
-        s.bind((host, port)) # bind the port 5000 to the socket, host is blank so we can accept connection from other computer in the network
+        s.bind((host, port)) # bind the port 5000 and host to the socket
+        
         print("Socket successfully created")
         print("Socket binded to ", port)
-        s.listen()
+        
+        s.listen() # wait for computers to connect
+        
         print("socket is listening")
         return s
     
@@ -21,16 +25,16 @@ def setupServer():
 
 # function that converts a list of strings to pigLatin
 def pigLatin(text):
-    deconstruct = []
-    translated = ""
+    deconstruct = [] # a list of words when the input text is a sentence/phrase/ more than one word delimited by spaces
+    translated = "" # holder for the translated word
     if " " in text: # if the input is a sentence
         deconstruct = text.split(" ")
 
-        for word in deconstruct: # translate to piglatin every word
+        for word in deconstruct: # translate each word to piglatin
             translated = translated + pigLatinHelper(word)
             translated = translated + " "
 
-    else: 
+    else: # if there is only one word in the input
         translated = pigLatinHelper(text)
 
     return translated
@@ -45,29 +49,26 @@ def isAllConsonant(text):
 # #function that converts input text to piglatin
 def pigLatinHelper(text):
 
-    normalized_text = text.lower()
-    translation = normalized_text
+    translation = text
 
     conso_clust = "" # consonant cluster
 
     # if first letter is a consonant
-    if normalized_text[0] not in ["a", "e", "i", "o", "u"]:
-        if isAllConsonant(normalized_text):
-            translation = translation[len(translation)-1] + translation[0:len(translation)-1]
+    if text[0] not in ["a", "e", "i", "o", "u"]:
+        if isAllConsonant(text): # if all letters in the word is a consonant (e.g. my, rhythms)
+            translation = translation[len(translation)-1] + translation[0:len(translation)-1] # we only get the consonant cluster up until the second to the last
             translation = translation + "ay"
-        else:
-            for letter in normalized_text:
-                if letter not in ["a", "e", "i", "o", "u"]:
+        else: # otherwise
+            for letter in text:
+                if letter not in ["a", "e", "i", "o", "u"]: # collect the cluster
                     conso_clust = conso_clust + letter
                     
                     translation = translation[1:len(translation)]
-                else:
+                else: # when a vowel is encountered
                     translation = translation + conso_clust
                     translation = translation + "ay"
                     break
-        # translation = translation + conso_clust
-        # translation = translation + "ay"
-    else:
+    else: # if the first letter is a vowel, we simply add yay at the end
         translation = translation + "yay"
     
 
@@ -75,20 +76,24 @@ def pigLatinHelper(text):
 #* ===================================================
 
 s = setupServer()
+
+#prompt messages to be sent
 message = "Successfully connected to the server"
-prompt = "Please enter a word to translate to Pig Latin: "
+prompt = "What do you want to translate to Pig Latin? : "
 cont = "Continue? (y/n): "
+
+
 while True:
     
-    c, addr = s.accept() # establish connection with client
+    c, addr = s.accept() # establish connection with a client
     print("Got connection from ", addr)
     c.send(message.encode()) #acknowledge client of the connection
 
     proceed = "y"
     while proceed != "n":
-        c.send(prompt.encode())
+        c.send(prompt.encode()) # please enter a word
         input_text = c.recv(1024) # wait for an input
-        input_text = input_text.decode()
+        input_text = input_text.decode() # decode received input
         print("Input: ", input_text)
 
         trans_text = pigLatin(input_text)
@@ -97,26 +102,5 @@ while True:
         c.send(trans_text.encode()) # send translated word to client
         c.send(cont.encode()) # send continue? prompt
         proceed = c.recv(1024).decode()
-    # c.close()
 
-
-
-# while(connected):
-#     # get input from client
-#     text_input = input()
-
-#     # check if input is valid
-#         # ! if invalid, get another input from user
-#     translation = pigLatin(text_input)
-#         # print input and translation
-#     print("Input Text: ", text_input, "\nPig Latin Translation: ", translation)
-
-#     # ! send translation
-#     # ask if continue translating
-#     connected = input("Continue? (y/n): ")
-
-#     # if not, terminate program
-#     if(connected == "n" or connected == "no"):
-#         connected=False
-#         # ! send this to client
-#         print("Bye!")
+c.close()
